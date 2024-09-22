1bfe3adeb55a7a464c8e4e5bf6ffd62b029e8088
OlexandrMykhalchenko <hialexxxxx@gmail.com>
2024-09-22 19:46:49 +0300

    Third commit

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -3,8 +3,14 @@
 from django.urls import path
 from . import views
 
-app_name = 'Learning_logs'
+
+app_name = 'learning_logs'
 urlpatterns = [
     # Домашняя страница
     path('', views.index, name='index'),
+    # Страница со списком всех тем
+    path('topics/', views.topics, name='topics'),
+
+    # Страница с подробной информацией по отдельной теме
+    path('topics/<int:topic_id>/', views.topic, name='topic'),
 ]
